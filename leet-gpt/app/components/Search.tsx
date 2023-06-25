032189e8ddf5e082520108554af7e6dc85a864eb
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-06-25 12:11:58 -0500

    Added basic form structure

--- leet-gpt/app/components/Search.tsx
+++ leet-gpt/app/components/Search.tsx
@@ -1,10 +1,33 @@
+"use client"
+
 import React from 'react'
 import Result from './Result'
+import { useState } from 'react'
 
 export default function Search() {
+    const [value, setValue] = useState('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(`change: ${event.target.name}`);
+        setValue(event.target.name)
+    }
+
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        //send the submit value to open AI API and async await get the value
+
+        //after we get the value from open AI we need to send this to the result component
+    }
+
+
   return (
     <div>
         Search
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='enter the problem text' onChange={handleChange}/>
+            <br />
+            <button type='submit'>Search</button>
+        </form>
         <Result searchText={'Hello I am search text'}/>
     </div>
   )
