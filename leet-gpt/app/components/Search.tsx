cc65215efcae03a815196da9541b2d3124016fba
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-06-27 21:37:21 -0500

    Added some form functionality

--- leet-gpt/app/components/Search.tsx
+++ leet-gpt/app/components/Search.tsx
@@ -5,18 +5,28 @@ import Result from "./Result";
 import { useState } from "react";
 
 export default function Search() {
-  const [value, setValue] = useState("");
+  const [formData, setFormData] = useState({ type: "", prompt: "" });
+  const [selectedVal, setSelectedVal] = useState("");
+  const [searchVal, setSearchVal] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(`change: ${event.target.name}`);
-    setValue(event.target.name);
+    setSearchVal(event.target.name);
+  };
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`radio value: ${event.target.value}`);
+    setSelectedVal(event.target.name);
   };
 
   const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
-    //send the submit value to open AI API and async await get the value
-    setValue("text");
-    //after we get the value from open AI we need to send this to the result component
+    setFormData({
+      type: selectedVal,
+      prompt: searchVal,
+    });
+
+    console.log(formData);
   };
 
   return (
@@ -30,27 +40,46 @@ export default function Search() {
           placeholder="enter your leet code problem"
           onChange={handleChange}
           className="py-4 mt-2 border border-gray-300 rounded-md px-7 focus:outline-none focus:border-green-500 "
+          required
         />
 
         <div className="flex justify-center row-span-2 pt-4 align-middle gap-7">
           <div>
-            <input type="radio" id="hint" name="type" value="hint" checked />
-            <label htmlFor="huey">Hint</label>
+            <label htmlFor="hint">
+              <input
+                type="radio"
+                id="hint"
+                name="type"
+                value="hint"
+                onChange={handleRadioChange}
+                required
+              />
+              Hint
+            </label>
           </div>
           <div>
-            <input type="radio" id="solution" name="type" value="solution" />
-            <label htmlFor="huey">Solution</label>
+            <label htmlFor="solution">
+              Solution
+              <input
+                type="radio"
+                id="solution"
+                name="type"
+                value="solution"
+                onChange={handleRadioChange}
+                required
+              />
+            </label>
           </div>
         </div>
         <br />
         <button
-          className="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-600 max-w-xs:"
+          className="max-w-sm px-4 py-2 font-bold text-white bg-green-500 border-b-4 border-green-700 rounded w hover:bg-green-400 hover:border-green-500"
           type="submit"
         >
           Search
         </button>
       </form>
-      {value ? <Result searchText={"Hello I am search text"} /> : ""}
+      {formData ? <Result searchText={"Hello I am search text"} /> : ""}
     </div>
   );
 }
