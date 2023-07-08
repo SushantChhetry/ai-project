3c4a2f47f2e7f12a6f0ce6eac805a0434e97c15a
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-08 12:09:43 -0500

    Fixed the prompt in openAI api

--- leet-gpt/utils/openApi.tsx
+++ leet-gpt/utils/openApi.tsx
@@ -1,7 +1,9 @@
 const { Configuration, OpenAIApi } = require("openai");
 
+const apiKey = process.env.OPENAI_API_KEY;
+
 const config = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 //to test this cd utils , then node openApi.tsx
@@ -11,10 +13,10 @@ const openai = new OpenAIApi(config);
 const runPrompt = async () => {
   //here we gave the ai a more detailed prompt with the strucutre we want
   const prompt = `
-        Write me a joke about a cat and a bowl of pasta. Return reponse in the following parsable JSON format: 
+        Give me a 500 char hint on solving the merge sorted arry leetcode question in the following formatting: 
         {
-            "Q": "question",
-            "A": "answer"
+            "Q": "link to the leet code problem",
+            "A": "hint to the leet code problem"
         }
     `;
 
@@ -25,7 +27,7 @@ const runPrompt = async () => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: prompt,
-    max_tokens: 100,
+    max_tokens: 500,
     temperature: 0.6,
   });
 
@@ -34,8 +36,10 @@ const runPrompt = async () => {
   const parasableJSONResponse = response.data.choices[0].text;
   const parsedResponse = JSON.parse(parasableJSONResponse);
 
-  console.log("Question: ", parsedResponse.Q);
-  console.log("Answer: ", parsedResponse.A);
+  console.log(parsedResponse);
+
+  //   console.log("Question: ", parsedResponse.Q);
+  //   console.log("Answer: ", parsedResponse.A);
 };
 
 export default runPrompt;
