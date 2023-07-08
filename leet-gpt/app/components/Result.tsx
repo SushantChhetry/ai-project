5b2b57bfc865edadcbdbe046d54b955b5da619be
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-08 11:34:57 -0500

    Added openAI function

--- leet-gpt/app/components/Result.tsx
+++ leet-gpt/app/components/Result.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import runPrompt from "../../utils/openApi";
 
 type Props = {
   searchText: any;
 };
 
 export default function Result({ searchText }: Props) {
-  console.log(searchText);
+  const [search, setSearch] = useState(searchText);
+
+  useEffect(() => {
+    console.log(`component rendered`);
+    console.log(search);
+    runPrompt();
+  }, [search]);
+
   return (
-    <div className=" font-mono h1- font-bold flex justify-center py-4 bg-gray-300 my-4">
+    <div className=" font-mono h1- font-bold flex justify-center py-4 bg-gray-300 my-4 flex-col items-center">
       <div className="">Result</div>
-      {searchText.prompt}
+      <br />
+      {search.prompt}
     </div>
   );
 }
--- leet-gpt/utils/openApi.tsx
+++ leet-gpt/utils/openApi.tsx
@@ -1,17 +1,16 @@
-const {Configuration, OpenAIApi}  = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
-    apiKey: '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'
+  apiKey: "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",
 });
 
 //to test this cd utils , then node openApi.tsx
 
-
 const openai = new OpenAIApi(config);
 
 const runPrompt = async () => {
-    //here we gave the ai a more detailed prompt with the strucutre we want 
-    const prompt = `
+  //here we gave the ai a more detailed prompt with the strucutre we want
+  const prompt = `
         Write me a joke about a cat and a bowl of pasta. Return reponse in the following parsable JSON format: 
         {
             "Q": "question",
@@ -19,24 +18,24 @@ const runPrompt = async () => {
         }
     `;
 
-    const hintPrompt = `` //prompt for hint search
-    
-    const solutionPrompt = `` //prompt for solution search
+  const hintPrompt = ``; //prompt for hint search
 
-    const response = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt: prompt,
-        max_tokens: 100,
-        temperature: 0.6,
-    });
+  const solutionPrompt = ``; //prompt for solution search
 
-    console.log(response.data);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 100,
+    temperature: 0.6,
+  });
 
-    const parasableJSONResponse = response.data.choices[0].text;
-    const parsedResponse = JSON.parse(parasableJSONResponse);
+  console.log(response.data);
 
-    console.log("Question: " , parsedResponse.Q);
-    console.log("Answer: ", parsedResponse.A)
+  const parasableJSONResponse = response.data.choices[0].text;
+  const parsedResponse = JSON.parse(parasableJSONResponse);
+
+  console.log("Question: ", parsedResponse.Q);
+  console.log("Answer: ", parsedResponse.A);
 };
 
-runPrompt();
+export default runPrompt;
